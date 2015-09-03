47d5586392d5827a16e4ef053a060eb17f6b7b5f
Hasan Khan <slashdot10@gmail.com>
2015-09-03 00:20:50 -0700

    no need to merge with ICOE times, deleted merge.js, deleted output.json, add new maps times

--- maps/convert.js
+++ maps/convert.js
@@ -1,11 +1,22 @@
-xlsxj = require("xlsx-to-json");
-  xlsxj({
-    input: "PrayerTime2015MAPS.xlsx", 
-    output: "SalahSchedules_MAPS.json"
-  }, function(err, result) {
-    if(err) {
-      console.error(err);
-    }else {
-      console.log(result);
-    }
+var _ = require('underscore'),
+  fs = require('fs'),
+  xlsxj = require("xlsx-to-json");
+
+xlsxj({
+  input: "PrayerTime2015MAPS_2.xlsx",
+  output: "SalahSchedules_MAPS.json"
+}, function (err, result) {
+  if (err) {
+    console.error(err);
+  } else {
+    convert(result);
+    console.log(result);    
+  }
+});
+
+function convert(result) {
+  result.forEach(function (item) {
+    item.day = parseInt(item.day);
   });
+  fs.writeFileSync('SalahSchedules.json', JSON.stringify(result));
+}
